ee9ea3da1a0db2731e23546d9ddbf8d0045da913
SaifUllahKhan <saifullah2019@namal.edu.pk>
2024-11-27 12:20:51 +0500

    latest

--- New_data_flow_design/app.py
+++ New_data_flow_design/app.py
@@ -8,9 +8,13 @@ from _kafka import Consumer
 from aiokafka import ConsumerRecord
 import config
 from elasticsearch import Elasticsearch,helpers
-from models import KBCDCPayloadSchema, KBCDCSchema, URL
+from models import KBCDCPayloadSchema, KBCDCSchema, URL , Domain , IP ,Object
 import time
 from datetime import datetime
+import jmespath
+
+
+
 logger = get_logger(__name__ , config.DEBUG)
 loop = asyncio.get_event_loop()
 es = Elasticsearch([{'host': 'localhost', 'port': 9200 ,  'scheme': 'http'}],
@@ -24,11 +28,12 @@ except Exception as e:
     print(f"Error connecting to Elasticsearch: {e}")
 
 
-async def process_data(data):
+async def process_data(data , type ):
 
-    data["_id"] = data["raw"][0].get("id")
-    data["id"] = data["raw"][0].get("id")
-    data["srid"] = data.get("srid" , "url" +  str(uuid.uuid4()))
+    # Intel
+    data["_id"] = data["raw"][0].get("id", None)
+    data["id"] = data["raw"][0].get("id",None)
+    data["srid"] = type + "-" +   data.get("srid" , str(uuid.uuid4()))
     data["uid"] = str(uuid.uuid4())
     data["name"] = data["value"]
     data["displayName"] = data["value"]
@@ -39,23 +44,23 @@ async def process_data(data):
     data["updated_at"] = data.get("updated_at", date)
     data["entity"] = ["url"]
     data["tag"] = data["tags"]
-    data["source"] = data["sources"]
+    data["source"] = jmespath.search("sources[].name", data=data)   # some source repeat soome name appear 2 times?
+    logger.info(f"the info {data["source"]} "  )
     data["strikes_count"] = len(data["strikes"])
 
     tts = data["mitre_ttp"]
     tactics = []
     techniques = []
-
     for tactic in tts:
         tactics.append({"mitre_id" : tactic["tac_id"] , "name" : tactic["tac_name"] })
         for tech in tactic["tech"]:
             technique = {"mitre_id" : tech["tech_id"] , "name" : tech["tech_name"] }
             if technique not in techniques:
                 techniques.append(technique)
-
     data["tactic"] = tactics
     data["technique"] = techniques
     data["strike_id"] = data["strikes"]
+
 
     # Artifact Class Properties
     data["source_id"] = [data["sources"][0].get("id")] #
@@ -76,59 +81,155 @@ async def process_data(data):
     data["analysis_state"] = data.get("analysis_state" ,None)
     data["community_verdict"] = data.get("community_verdict" ,None)
     data["community_score"] = data.get("community_score" ,None)
-    data["free_verdict"] = data.get("analysis", None).get("verdict",None)[0].get("value" , None)
+    data["free_verdict"] = data.get("analysis", {}).get("verdict",{})[0].get("value" , None)
     data["free_score"] = data.get("free_score", None)
     data["premium_verdict"] = data.get("premium_verdict", None)
     data["premium_score"] = data.get("premium_score", None)
     data["strike_ready_verdict"] = data.get("verdict", None)
     data["strike_ready_score"] = data.get("score", None)
-    data["analyst_score"] = int(data.get("analysis", None).get("score",None)[0].get("value" , None))
+    data["analyst_score"] = data.get("analyst_score", None ) # int(data.get("analysis", None).get("score",None)[0].get("value" , None))
     data["analyst_updated_at"] = data.get("analyst_updated_at", None)  #
     data["kb_verdict"] = data.get("kb_verdict", None) #
     data["kb_score"] = data.get("kb_score", None) #
     data["malware"] = data.get("malwares", None) 
     data["strike"] = data.get("strikes", None)
     data["tool"] = data.get("tools", None)
-    data["cve"] = [data.get("vulnerability", None)[0].get("reported_cves")]
-    data["category"]= [data.get("host",None).get("category", None)]
-    data["threat_actor"] = data.get("threat_actor", None)
-    data["threat_actors"] = data.get("threat_actors", None)
+    data["cve"] = [data.get("vulnerability", {})[0].get("reported_cves")]
+    data["category"]= [data.get("host",{}).get("category", None)]
+    # data["threat_actor"] = data.get("threat_actor", None)
+    data["threat_actors"] = jmespath.search("threat_actors[*].value" , data =data)
     data["ransom"] = data.get("ransom", None)
-    data["targeted_organization"] = data.get("associated_threat_campaigns")[0].get("targeted_organization", None)
-    data["targets_sector"] = data.get("associated_threat_campaigns")[0].get("targets_sector", None)
-    data["targeted_industry"] = data.get("associated_threat_campaigns")[0].get("targeted_industry", None)
-    data["targets_region"] = data.get("associated_threat_campaigns")[0].get("targets_region", None)
-    data["targeted_country"] = data.get("associated_threat_campaigns")[0].get("targeted_country", None)
-    data["attack_origin"] = data.get("associated_threat_campaigns")[0].get("attack_origin", None)
-    data["source_type"] = [data["sources"][0].get("type")] #
+
+    data["targeted_organization"] = jmespath.search("associated_threat_campaigns[*].targeted_organization", data = data)
+    data["targets_sector"] = jmespath.search("associated_threat_campaigns[*].targets_sector", None)
+    data["targeted_industry"] = jmespath.search("associated_threat_campaigns[*].targeted_industry", data =data) or None
+    data["targets_region"] = jmespath.search( "associated_threat_campaigns[*].targets_region", data = data ) or None
+    data["targeted_country"] = jmespath.search("associated_threat_campaigns[*].targeted_country", data =data ) or None
+    data["attack_origin"] = jmespath.search("associated_threat_campaigns[*].attack_origin", data = data ) or None
     data["whois"] = data.get("whois", None)
     data["tlp"] = [data.get("tlp", None)]
 
-    urls = []
-    for source in  data["sources"]:
-        url = source.get("url")
-        if url:
-            urls.append(url)
-    # URL Class Properties
-    data["url"] = urls if len(urls) > 0 else None
-    data["object"] = data.get("object" ,None)
-    data["email"] = None
-    data["domain"] =[ data.get("host",None).get("value",None)]
-    data["ip"] = data.get("ip",None)
+
+
+
+    if type == "url":
+        data["ip"] = data.get("ip",None)
+
+    elif type == "domain":
+        data["domain"] =[ data.get("host", {}).get("value",None)] #
+        data["sr_rank"] =  data.get("sr_rank" , None)#
+        data["md5"] = data.get("md5" , None) #
+        data["sha1"] = data.get("sha1" , None) #
+        data["sha256"]= data.get("sha256" , None) #
+        ipv4_list = []
+        ips = data.get("infrastructure_info" ,None).get("resolving_ip" , None)
+        for ip in ips:
+             ipv4 = ip.get("ip" ,None)
+             if ipv4:
+                ipv4_list.append(ipv4.get("value"))
+
+        data["ipv4"] = ipv4_list
+        
+        data["email"] = data.get("emails" ,None)
+
+    elif type == "ipv4":
+        data["ip"] = data.get("ip" , None)
+        data["ip_type"] = data.get("ip_type" , None)
+        data["md5"] = data.get("md5" , None) #
+        data["sha1"] = data.get("sha1" , None) #
+        data["sha256"]= data.get("sha256" , None) 
+
+    elif type =="object":
+        data["md5"] = data.get("md5" , None) 
+        data["sha1"] = data.get("sha1" , None) 
+        data["sha256"]= data.get("sha256" , None)  
+        data["sha512"] = data.get("sha512" , None)
+        data["node_type"] = data.get("node_type" , None)
+        data["size"] = data.get("object_info", {}).get("file_size",None)
+        data["file_type"] = [data.get("object_info", {}).get("file_type",None) ]  
+        objects =[]
+        if data["md5"] is not None:
+            objects.append( data["md5"])
+        if data["sha1"] is not None:
+            objects.append( data["sha1"])
+        if data["sha256"] is not None:
+            objects.append( data["sha256"])
+        if data["sha512"] is not None:
+            objects.append( data["sha512"]) 
+
+        data["object"] = objects
+
+    if type in [ "domain" , "url" ,  "ipv4"]:
+        data_objects_list = data.get("files_downloaded" , None) # +  data.get("communicating_files" , None)    
+        objects = []
+        for data_object in data_objects_list:
+            object = data_object.get("object",None)
+            if object:
+                sha1 =  object.get("sha1" ,None)
+                if sha1 != None and sha1 != " ":
+                            if sha1 not in objects:
+                                objects.append(sha1)
+
+                sha256 =  object.get("sha256" ,None)
+                if sha256 != None and sha256 != " ":
+                            if sha256 not in objects:
+                                objects.append(sha256)
+
+                sha512 =  object.get("sha512" ,None)
+                if sha512 != None and sha512 != " ":
+                            if sha512 not in objects:
+                                objects.append(sha512)
+
+                md5 =  object.get("md5" ,None)
+                if md5 != None and md5 != " ":
+                            if md5 not in objects:
+                                objects.append(md5)
+
+        data["object"] = objects
+
+        urls = []
+        for source in  data["sources"]:
+            url = source.get("url")
+            if url:
+                urls.append(url)
+
+        # URL Class Properties
+        data["url"] = urls if len(urls) > 0 else None
+
+        data["email"] = data.get("emails" ,None)   
+        data["domain"] =[ data.get("host", {}).get("value",None)]  # need to solve
+
+
+
     return data 
+
+index_mapper = {
+    "url" : "url_data",
+    "domain" : "domain_data",
+    "ipv4" : "ipv4_data",
+    "object" :"object_data"
+}
+
+
+model_mapper = {
+    "url" : URL,
+    "domain" : Domain,
+    "ipv4" : IP,
+    "object" :Object
+}
 consumer = Consumer(config.KAFKA_BOOTSTRAP_SERVERS , loop=loop)
 @consumer.consume([config.TOPIC] , group_id=config.KAFKA_INTERNAL_GROUP)
 async def process_logs(message: ConsumerRecord):
 
     try:
         data = message.value
-        pro_data = await process_data(data)
-        logger.info(pro_data)
-        body = URL(**pro_data)
-        
-        index = "url_data"
-        res = es.index(index=index, body=body.dict(exclude_none=True)) 
-        logger.info(f' the responce after saving the data is : - {res} ')
+        type = data.get("type")
+        pro_data = await process_data(data , type)
+        index = index_mapper.get(type)
+        model = model_mapper.get(type)
+        body = model(**pro_data)
+        res = es.index(index=index, id=data.get("_id"),  body=body.dict(exclude_none=True)) 
+        # logger.info(f' the responce after saving the data is : - {res} ')
     except Exception as e:
        logger.exception(e)
 if __name__ == '__main__':
--- New_data_flow_design/kafka_producer.py
+++ New_data_flow_design/kafka_producer.py
@@ -26718,6 +26718,7 @@ obj_use_mal = {
     }
 }
 
+
 data ={
 	"meta": {
 		"timestamp": 1690206793348,
@@ -26862,6 +26863,8 @@ data ={
 }
 
 
+topic  = "elastic"
+
 url_event =  {
     "value": "https://vk.com/doc418490229_669356461?hash=QZ4Z4X5d1YyJIYsuybrCPLzO6Ls2spy0JgNSqnc72tL&dl=0LhPUEBSvCJyNIPscRw3GxYsokIzwdbZxqgmCthcoz8&api=1&no_preview=1#xin",
     "type": "url",
@@ -26951,6 +26954,7 @@ url_event =  {
     ],
     "first_seen": "2023-12-08T21:50:03.600000",
     "last_seen": "2023-12-08T21:50:03.600000",
+    "update_at": "2023-12-08T22:01:18.586065",
     "raw": [
       {
         "urlhaus": {
@@ -27148,6 +27152,7 @@ url_event =  {
     ],
     "is_fp": None
   }
+
 
 domain_event = {
         "value": "metamask.tokenim.ink",
@@ -27507,8 +27512,425 @@ domain_event = {
         ]
     }
 
-
-topic  = "elastic"
+ip_event = {
+        "value": "45.86.74.243",
+        "type": "ipv4",
+        "confidence": "medium",
+        "sources": [
+            {
+                "id": "26",
+                "name": "rstcloud",
+                "display_name": "RST Cloud",
+                "url": "https://raw.githubusercontent.com/rstcloud/rstthreats/master/feeds/full/random100_ioc_ip_latest.json",
+                "confidence": "medium",
+                "type": "open_feed",
+                "time": "2023-12-13T00:03:37.288355"
+            }
+        ],
+        "score": 68,
+        "verdict": "suspicious",
+        "approved": False,
+        "malwares": [
+            {
+                "reported_source": {
+                    "name": "rstcloud",
+                    "sr_reviewed": None,
+                    "reported_time": "2024-01-07T11:31:21.220030",
+                    "type": "open_feed",
+                    "confidence": "high"
+                },
+                "value": "Redline Stealer"
+            }
+        ],
+        "vulnerability": [
+            {
+                "value": "Follina",
+                "reported_cves": "CVE-2022-0732",
+                "reported_source": {
+                    "name": "rstcloud",
+                    "reported_time": "2024-02-15T21:21:30.153664",
+                    "confidence": "low",
+                    "type": "open_feed",
+                    "sr_reviewed": None
+                }
+            }
+        ],
+        "threat_actors": [
+            {
+                "value": "LV Ransomware Group",
+                "reported_source": {
+                    "name": "rstcloud",
+                    "reported_time": "2023-03-29T11:36:56.951523",
+                    "confidence": "high",
+                    "type": "open_feed",
+                    "sr_reviewed": None
+                }
+            }
+        ],
+        "threat": [
+            {
+                "stage": "",
+                "reported_source": {
+                    "name": "rstcloud",
+                    "reported_time": "2023-12-02T19:25:03.278393",
+                    "confidence": "high",
+                    "type": "open_feed",
+                    "sr_reviewed": None
+                },
+                "threat_type": "phishing"
+            }
+        ],
+        "tools": [
+            {
+                "reported_source": {
+                    "sr_reviewed": None,
+                    "confidence": "high",
+                    "name": "rstcloud",
+                    "reported_time": "2023-12-02T15:10:33.417572",
+                    "type": "open_feed"
+                },
+                "value": "Beacon"
+            }
+        ],
+        "strikes": [
+            "STA4090"
+        ],
+        "tags": [
+            "cobalt_strike",
+            "malware"
+        ],
+        "first_seen": "2023-03-13T00:00:00.909823",
+        "last_seen": "2023-12-10T00:00:00.746659",
+        "raw": [
+            {
+                "RST_Cloud_IP": {
+                    "ip": {
+                        "v4": "45.86.74.243",
+                        "num": 760630003
+                    },
+                    "ports": [
+                        8080
+                    ],
+                    "fseen": 1678665600,
+                    "lseen": 1702166400,
+                    "collect": 1702252800,
+                    "src": {
+                        "name": [
+                            "github_repos"
+                        ],
+                        "report": "https://github.com/stamparm/maltrail"
+                    },
+                    "tags": {
+                        "str": [
+                            "malware"
+                        ],
+                        "codes": [
+                            10
+                        ]
+                    },
+                    "asn": {
+                        "num": 44477,
+                        "firstip": {
+                            "netv4": "45.86.74.0",
+                            "num": 760629760
+                        },
+                        "lastip": {
+                            "netv4": "45.86.79.255",
+                            "num": 760631295
+                        },
+                        "cloud": "",
+                        "domains": 55899,
+                        "org": "",
+                        "isp": "STARKINDUSTRIES"
+                    },
+                    "geo": {
+                        "city": "Manila",
+                        "country": "Philippines",
+                        "region": "Metro Manila"
+                    },
+                    "related": {
+                        "domains": []
+                    },
+                    "score": {
+                        "total": 6,
+                        "src": 68.04,
+                        "tags": 0.89,
+                        "frequency": 0.1
+                    },
+                    "fp": {
+                        "alarm": "False",
+                        "descr": ""
+                    },
+                    "threat": [
+                        "cobalt_strike"
+                    ],
+                    "cve": [],
+                    "industry": [],
+                    "ttp": [],
+                    "id": "90e8a60a-a026-30fc-9d4e-33f4f361ce5b",
+                    "title": "RST Threat feed. IOC: 45.86.74.243",
+                    "description": "IOC with tags: malware. Related threats: cobalt_strike"
+                }
+            }
+        ],
+        "analysis": {
+            "status": [],
+            "verdict": [
+                {
+                    "value": "suspicious",
+                    "reported_source": {
+                        "name": "rstcloud",
+                        "reported_time": "2023-12-13T00:03:37.288491",
+                        "confidence": "medium",
+                        "type": "open_feed",
+                        "sr_reviewed": False
+                    }
+                }
+            ],
+            "confidence": [
+                {
+                    "value": "medium",
+                    "reported_source": {
+                        "name": "rstcloud",
+                        "reported_time": "2023-12-13T00:03:37.288537",
+                        "confidence": "medium",
+                        "type": "open_feed",
+                        "sr_reviewed": False
+                    }
+                }
+            ],
+            "score": [
+                {
+                    "value": "68.04",
+                    "reported_source": {
+                        "name": "rstcloud",
+                        "reported_time": "2023-12-13T00:03:37.288567",
+                        "confidence": "medium",
+                        "type": "open_feed",
+                        "sr_reviewed": False
+                    }
+                }
+            ]
+        },
+        "presence_index": {},
+        "mitigation_rules": [],
+        "first_crawled": "2023-12-13T00:03:37.288582",
+        "last_crawled": "2023-12-13T00:03:37.288582",
+        "mitre_ttp": [
+            {
+                "tac_id": "TA0008",
+                "tac_name": "Lateral Movement",
+                "tech": [
+                    {
+                        "tech_id": "T1550",
+                        "tech_name": "Privilege Escalation",
+                        "sub_tech": [
+                            {
+                                "sub_tech_id": ".002",
+                                "sub_tech_name": "Powershell"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ],
+        "tlp": "green",
+        "whitelisted_check": True,
+        "internal_raw_osint": False,
+        "internal_allow_sync": True,
+        "associated_threat_campaigns": [
+            {
+                "campaign_name": "Google Breach",
+                "attack_origin": [
+                    "Russia"
+                ],
+                "targeted_region": [
+                    "North America"
+                ],
+                "sources": [
+                    {
+                        "confidence": "medium",
+                        "name": "open_phish",
+                        "id": "10",
+                        "time": "2023-12-25T08:50:27.882234",
+                        "display_name": "OpenPhish",
+                        "type": "open_feed",
+                        "url": "https://www.openphish.com/"
+                    }
+                ],
+                "targeted_country": [
+                    "us"
+                ],
+                "targeted_industry": [
+                    "Finance"
+                ],
+                "target": "AT&T Inc.",
+                "targeted_sector": [
+                    "Finance"
+                ]
+            }
+        ],
+        "status": "offline",
+        "infra_info": {},
+        "location_info": {
+            "continent_code": "NA",
+            "continent_name": "North America",
+            "country_code": "US",
+            "country_name": "United States",
+            "country_is_in_european_union": None,
+            "city": "Los Angeles",
+            "region": "",
+            "region_code": "",
+            "country_code_iso3": "",
+            "country_capital": "",
+            "country_tld": "",
+            "postal": "90009",
+            "latitude": 34.0544,
+            "longitude": -118.244,
+            "timezone": "America/Los_Angeles",
+            "utc_offset": "",
+            "country_calling_code": "",
+            "currency": "",
+            "currency_name": "",
+            "languages": [],
+            "country_area": None,
+            "country_population": None
+        },
+        "ip_type": "public",
+        "ip_class": "A",
+        "ip_history": [],
+        "infra_security": {},
+        "os_fingerprinting": {},
+        "subnet_info": {
+            "subnet": "45.86.64.0/20",
+            "subnet_allocation_age": None,
+            "subnet_allocation_date": "",
+            "subnet_reputation": None,
+            "subnet_reputation_score": None,
+            "subnet_density": {}
+        },
+        "asn_info": {
+            "asn": 35913,
+            "asn_allocation_age": None,
+            "asn_allocation_date": "",
+            "asn_rank": None,
+            "asn_rank_score": None,
+            "asn_reputation": None,
+            "asn_reputation_score": None,
+            "asn_takedown_reputation": None,
+            "asn_takedown_reputation_score": None,
+            "asname": "DEDIPATH-LLC",
+            "date": "",
+            "density": None,
+            "ips_in_asn": None,
+            "ips_num_active": None,
+            "ips_num_listed": None,
+            "asn_reputation_explanation": {}
+        },
+        "open_ports": [
+            {
+                "port": 8080,
+                "type": ""
+            }
+        ],
+        "host": {
+            "value": "45.86.74.243",
+            "type": "ipv4",
+            "category": "ip",
+            "port": None
+        },
+        "content_serving": {},
+        "files_downloaded": [
+            {
+                "object": {
+                    "sha1": "",
+                    "sha256": "283c4f9174d19944189f5d6961f3319420c752bfaa6f65c19dd85ad2ca1cd77f",
+                    "object_info": {},
+                    "sha3_384": "",
+                    "ss_deep": "",
+                    "sha512": "",
+                    "tlsh": "",
+                    "vhash": "",
+                    "imp_hash": "",
+                    "telfhash": "",
+                    "md5": ""
+                }
+            }
+        ],
+        "communicating_files": [
+            {
+                "object": {
+                    "sha1": "",
+                    "sha256": "283c4f9174d19944189f5d6961f3319420c752bfaa6f65c19dd85ad2ca1cd77f",
+                    "object_info": {},
+                    "sha3_384": "",
+                    "ss_deep": "",
+                    "sha512": "",
+                    "tlsh": "",
+                    "vhash": "",
+                    "imp_hash": "",
+                    "telfhash": "",
+                    "md5": ""
+                }
+            }
+        ],
+        "associated_urls": [
+            {
+                "url": {
+                    "value": "http://36.49.34.150:12225/.i"
+                }
+            }
+        ],
+        "ip_whois": {
+            "address": "",
+            "city": "",
+            "country": "",
+            "state": "",
+            "reverse_dns": [],
+            "creation_date": None,
+            "expiration_date": None,
+            "name": "45.86.74.243",
+            "epp_status": [],
+            "emails": [
+                "hostmaster@ripe.net",
+                "abuse@ripe.net"
+            ],
+            "whois_server": "",
+            "zip_code": None,
+            "name_servers": [],
+            "organization": "",
+            "registrar": {
+                "Registrar_WHOIS_Server": "",
+                "Registrar_url": "",
+                "Registrar_name": "",
+                "Registrar_organization": "",
+                "Registrar_location": {},
+                "Registrar_status": "",
+                "Registrar_IANA_id": ""
+            },
+            "referral_url": "",
+            "dnssec": [],
+            "registry_domain_url_id": "",
+            "updated_at": None
+        },
+        "isp": {
+            "isp_name": "Stark Industries Solutions LTD",
+            "country": "Philippines",
+            "date": "2023-12-13T00:03:36.804675"
+        },
+        "artifact_validation": [
+            {
+                "benign": False,
+                "whitelisted": False,
+                "is_deployable": True,
+                "date": "2023-12-13T00:03:37.288985",
+                "compromised": False,
+                "is_fp": False,
+                "forcefully_malicious": None,
+                "source": "whitelisting_service"
+            }
+        ]
+    }
 
 
 if __name__ == "__main__":
@@ -27516,6 +27938,6 @@ if __name__ == "__main__":
     # for k in range(150):
     #     for i in range(3):
     registered_user = object
-    response = producer.send(topic, url_event)
+    response = producer.send(topic, ip_event)
     print(response.get())
     time.sleep(5)
